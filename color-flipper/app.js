55d1de2dd4aa2dc860d3d458993af462b1162e96
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-01 19:38:33 +0600

    show color code

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -3,8 +3,14 @@ const getColor = () => {
 
     const randomCode = "#" + randomNumber.toString(16);
 
+    document.getElementById("color-code").innerText = randomCode;
+
     document.body.style.background = randomCode;
+
+    
 }
 
 
 document.getElementById("btn").addEventListener("click", getColor);
+
+getColor();
