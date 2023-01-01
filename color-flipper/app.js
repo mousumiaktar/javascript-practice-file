5074a81b397559cff26a6c091a95f24f68f2ff8f
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-01 19:45:59 +0600

    comment added

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,16 +1,23 @@
 const getColor = () => {
+    // RANDOM-NUMBER
     const randomNumber = Math.floor(Math.random() * 16777215);
 
+    // MAKE STRING THE RANDOM NUMBER
     const randomCode = "#" + randomNumber.toString(16);
 
+    // DISPLAY COLOR CODE CHANGE
     document.getElementById("color-code").innerText = randomCode;
 
+    // DISPLAY BACKGROUND COLOR
     document.body.style.background = randomCode;
 
+    // COPY COLOR CODE WITH CLICK
     navigator.clipboard.writeText(randomCode);
 }
 
-
+// EVENT CALL
 document.getElementById("btn").addEventListener("click", getColor);
 
+
+// INITIAL-CALL
 getColor();
